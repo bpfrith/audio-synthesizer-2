d9d54f4ee3222b0a35f03d6a42da0286974e2ead
Ben Frith <bpfrith@gmail.com>
2017-03-20 14:46:10 +0000

    Synth.jsx mostly completed, but subject to change.

--- client/src/components/Synth.jsx
+++ client/src/components/Synth.jsx
@@ -11,7 +11,7 @@ class Synth extends React.Component{
       lpf: {},
       amp: {}
     }
-  }
+  },
 
   componentDidMount(){
     var audioContext = new window.AudioContext
@@ -30,6 +30,7 @@ class Synth extends React.Component{
     oscillator.connect(filter)
     oscilllator.setWaveform("sine")
     oscillator.setFrequency(this.state.frequency || 261.63)
+    //may need to change to 440 for midi
     oscillator.setDetune(this.props.params.vcoDetune)
     oscillator.start(audioContext.currentTime)
 
@@ -39,5 +40,42 @@ class Synth extends React.Component{
       lpf: filter,
       amp:outputAmp
     })
+  },
+
+  componentWillReceiveProps(nextProps){
+    var event = this.state.context.currentTime
+    this.updateOscillator(nextProps.params.event);
+    this.updateNote(nextProps.params,event)
+    this.updateFilter(nextProprs.params, event)
+  },
+
+  updateOscillator(params, event){
+    this.state.osc.setDetune(params.vcoDeture, now)
+  },
+
+  updateNote(params, event){
+    var lastNotePlayed = params.notes[params.notes.length-1]
+    var noteFrequency = this.midiNoteToHz(lastNotePlayed)
+    this.state.osc.setFrequency(noteFreqency ||0)
+  },
+
+  updateFilter(params, now){
+    this.state.lpf.frequency.setValueAtTime(params.vcfResonance, event)
+  },
+
+  updateAmp(params, event){
+    this.state.amp.gain.setValueAtTime(params.level, event)
+  },
+
+  midiNoteToHz(midiNote){
+    return Math.pow(2, (midiNote - 69)/12)*261.63
+  },
+  //many need to change to 440
+
+  render(){
+    return null
   }
+
 }
+
+export default Synth
