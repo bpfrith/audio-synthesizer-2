b3cc91435cf9d0489223d3923e294e08be07dc9a
Ben Frith <bpfrith@gmail.com>
2017-03-20 17:33:16 +0000

    Put the Lin and Log adjusts into a filter container

---
+++ client/src/containers/Filter.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import LinAdjust from '../components/LinAdjust'
+import LogAdjust from '../components/LogAdjust'
+
+class LinAdjust extends React.Component{
+
+  handleCutoffChange(newFreq){
+    this.props.onChange({vcfResonance: newRes})
+  },
+
+  render(){
+    return (
+      <div className="synthmodule">
+        <LinAdjust
+          name = 'cutoff'
+          min = {20}
+          max = {20000}
+          onChange = {this.handleCutoffChange}
+          default = {this.props.cutoff}/>
+        <LogAdjust
+        name = 'res'
+        min = {1}
+        max = {30}
+        onChange = {this.handleResonanceChange}
+        default = {this.props.resonance}/>
+      </div>
+    )
+  }
+
+}
+
+export default Filter
